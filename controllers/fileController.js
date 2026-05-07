59a198aef9e344ff6505a8489ed77015faf34c16
Agrim <agrimdubey37@gmail.com>
2026-05-07 21:37:53 +0530

    feature: added a functioning endpoint storing files using multer

---
+++ controllers/fileController.js
@@ -0,0 +1,34 @@
+const File = require("../models/File");
+
+const uploadFile = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "No file uploaded",
+      });
+    }
+
+    const newFile = await File.create({
+      filename: req.file.filename,
+      originalname: req.file.originalname,
+      mimetype: req.file.mimetype,
+      size: req.file.size,
+      path: req.file.path,
+    });
+
+    res.status(201).json({
+      message: "File uploaded successfully",
+      file: newFile,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  uploadFile,
+};
